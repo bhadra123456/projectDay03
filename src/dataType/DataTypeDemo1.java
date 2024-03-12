03ea5a05d1816976e22dcff680c6f721a0974b87
bhadra12 <112570549+bhadra12@users.noreply.github.com>
2024-03-11 21:00:38 -0500

    My second  mmit

--- src/dataType/DataTypeDemo1.java
+++ src/dataType/DataTypeDemo1.java
@@ -8,6 +8,11 @@ public class DataTypeDemo1 {
 		int num2 = 22;
 		int sum = num1 + num2;
 		System.out.println("sum of num1 and num2 = " + sum);
+		
+		//done add day 2
+		System.out.println("The code is working now...");
+
+		
 
 	}
 
