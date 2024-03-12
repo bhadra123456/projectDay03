10f2fa11b65ead27523ca25058dfbb7611c69c1a
bhadra12 <112570549+bhadra12@users.noreply.github.com>
2024-03-11 20:50:19 -0500

    My first commit

---
+++ src/dataType/DataTypeDemo1.java
@@ -0,0 +1,14 @@
+package dataType;
+
+public class DataTypeDemo1 {
+
+	public static void main(String[] args) {
+		//integer data type ---> int
+		int num1 = 11;
+		int num2 = 22;
+		int sum = num1 + num2;
+		System.out.println("sum of num1 and num2 = " + sum);
+
+	}
+
+}
---
+++ src/dataType/DataTypeDemo2.java
@@ -0,0 +1,18 @@
+package dataType;
+
+public class DataTypeDemo2 {
+
+	public static void main(String[] args) {
+		// for storing character type of data we have to use -> char keyword
+		char ch = 'p';
+		String name = "Bhadra";
+		System.out.println(ch);
+		
+		System.out.println("--------");
+		int a = 45;
+		int b = 50;
+		boolean c = a<b;
+		System.out.println(c);
+		System.out.println(name);
+	}
+}
